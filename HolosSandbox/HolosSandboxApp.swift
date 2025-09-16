95fc49f0d2d75bd40994a93e1ec0235dd673f2a1
Sooraj Gupta <gupta297@wisc.edu>
2025-09-16 13:45:46 -0500

    Initial Commit

---
+++ HolosSandbox/HolosSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolosSandboxApp.swift
+//  HolosSandbox
+//
+//  Created by Patron on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct HolosSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
